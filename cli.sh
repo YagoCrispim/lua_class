3271850253fa409141f0528d601aef7cdd931706
Yago Crispim <34103098+YagoCrispim@users.noreply.github.com>
2023-12-21 09:21:28 -0300

    chore: add libs

---
+++ cli.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ACTION=$1
+
+case "$ACTION" in
+    example)
+        lua ./example.lua
+        ;;
+    test)
+        lua ./run_tests.lua
+        ;;
+    install)
+        LIBS_PATH="./lib"
+        REPOSITORIES=$(cat ./libs.txt)
+
+        for REPOSITORY in "${REPOSITORIES[@]}"
+        do
+            echo "----- Installing $REPOSITORY -----"
+            LIB_NAME=$(echo $REPOSITORY | cut -d'/' -f2 | cut -d'.' -f1)
+            LIB_PATH="$LIBS_PATH/$LIB_NAME"
+
+            if [ ! -d "$LIB_PATH" ]; then
+                git clone $REPOSITORY $LIB_PATH
+            fi
+            echo ""
+        done
+
+        ;;
+    *)
+        echo "Usage: $0 {test}"
+        exit 1
+esac
